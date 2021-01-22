3565af284ab455a60eee5e9928378235f7a211b7
Kevin Wittek <kevin@wittek.dev>
2021-01-22 16:18:01 +0100

    Add function for rotating.
    Co-authored-by: Neslihan Wittek <neslihanedes@gmail.com>

--- winkie/_nbdev.py
+++ winkie/_nbdev.py
@@ -6,6 +6,7 @@ index = {"DLCImporter": "00_dlc_importer.ipynb",
          "transform_to_relative": "00_dlc_importer.ipynb",
          "add_middle_neck": "00_dlc_importer.ipynb",
          "add_rotation": "00_dlc_importer.ipynb",
+         "apply_rotation": "00_dlc_importer.ipynb",
          "PigeonAnimator": "01_pigeon_animator.ipynb",
          "PigeonAnimatorFactory": "01_pigeon_animator.ipynb"}
 
